0bccd002856ab699523f692ac0d42f78fd807050
akumar120 <112678626+akumar120@users.noreply.github.com>
2024-10-26 18:32:43 +0530

    Updates

---
+++ precache-manifest.d4a5c2cec17a77e8b7705c0fe2dc1702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6579a482b7b2d2fe612a93c808fd978a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54aa544fc0756c04c098",
+    "url": "/static/css/main.0b3e064c.chunk.css"
+  },
+  {
+    "revision": "77ea0fc44de4f39b0549",
+    "url": "/static/js/2.832a6a9a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.832a6a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f6b689b3435993093a",
+    "url": "/static/js/3.331ae4c8.chunk.js"
+  },
+  {
+    "revision": "54aa544fc0756c04c098",
+    "url": "/static/js/main.18d73321.chunk.js"
+  },
+  {
+    "revision": "5a78cb2ea4dcda5ed26c",
+    "url": "/static/js/runtime-main.630a53cc.js"
+  }
+]);
